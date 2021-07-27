fa026ecd2eaf2de5f5f3cefa57f4a02adc459a0b
Maria Clara <clarinhab@gmail.com>
2021-07-27 20:07:22 -0300

    Insert changes for StoryPostButton and functions to add video

--- WordPress/WordPressUITests/Screens/ActionSheetComponent.swift
+++ WordPress/WordPressUITests/Screens/ActionSheetComponent.swift
@@ -2,15 +2,18 @@ import UITestsFoundation
 import XCTest
 
 class ActionSheetComponent: BaseScreen {
+    let storyPostbutton: XCUIElement
     let blogPostButton: XCUIElement
     let sitePageButton: XCUIElement
 
     struct ElementIDs {
+        static let storyPostButton = "storyButton"
         static let blogPostButton = "blogPostButton"
         static let sitePageButton = "sitePageButton"
     }
 
     init() {
+        storyPostbutton = XCUIApplication().buttons[ElementIDs.storyPostButton]
         blogPostButton = XCUIApplication().buttons[ElementIDs.blogPostButton]
         sitePageButton = XCUIApplication().buttons[ElementIDs.sitePageButton]
 
@@ -32,6 +35,13 @@ class ActionSheetComponent: BaseScreen {
         XCTAssert(sitePageButton.isHittable)
         sitePageButton.tap()
     }
+
+    func gotoStoryPost() {
+            XCTAssert(storyPostbutton.waitForExistence(timeout: 3))
+            XCTAssert(storyPostbutton.waitForHittability(timeout: 3))
+            XCTAssert(storyPostbutton.isHittable)
+            storyPostbutton.tap()
+        }
 
     static func isLoaded() -> Bool {
         return XCUIApplication().buttons[ElementIDs.blogPostButton].waitForExistence(timeout: 3)
